9825b6cf6a41a26841b6f736dd2c5ca8c668b19a
codingbycoding <wangyongjiancom@foxmail.com>
2013-04-04 20:06:36 +0800

    PC_ID in Makefile

--- test/testcpp.cpp
+++ test/testcpp.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-//#include "boost/lexical_cast.hpp"
+#include "boost/lexical_cast.hpp"
 
 int main()
 {
 	std::cout << "include iostream" << std::endl;
 	std::cout << "Hello https://code.google.com" << std::endl;
 
-	//using boost::lexical_cast;
-	//using boost::bad_lexical_cast;
+	using boost::lexical_cast;
+	using boost::bad_lexical_cast;
 
 	return 0;
 }
