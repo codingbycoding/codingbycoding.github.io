07f9ad0f4feb9aa9f3571c7c8c0ffc481b1dc872
unknown <TM77@.(none)>
2013-04-05 12:11:07 +0800

    cpp review

---
+++ review/cpp/cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	std::cout << "review cpp:" << std::endl;
+	
+	cout << "Run __FILE__:" << __FILE__ << " __LINE__:" << __LINE__ << " __DATE__:" << __DATE__ << " __TIME__:" << __TIME__ << endl;
+	return 0;
+}
