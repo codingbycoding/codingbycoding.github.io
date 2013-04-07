cc98d4a9b0841bacaee3a32140da749c2b41ed36
unknown <TM77@.(none)>
2013-04-07 22:32:01 +0800

    build and run in sublime

--- review/cpp/cpp.cpp
+++ review/cpp/cpp.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-
+#include <cassert>
+#include <fstream>
 using namespace std;
+
+class ArrayClass
+{
+public:
+	ArrayClass();
+	~ArrayClass();
+	bool operator ==(const ArrayClass&);
+	bool operator !=(const ArrayClass&);
+	ArrayClass& operator = (const ArrayClass&);
+};
+
 int main()
 {
-	std::cout << "review cpp:" << std::endl;
+	std::cout << "review cpp..." << std::endl;
 	
 	cout << "Run __FILE__:" << __FILE__ << " __LINE__:" << __LINE__ << " __DATE__:" << __DATE__ << " __TIME__:" << __TIME__ << endl;
+	
+	ofstream outfile("MakefileJ");
+	if( !outfile )
+	{
+		cerr << "ofstream failed..." << endl;
+	}
+	assert(1);
 	return 0;
 }
