ab69f840b9056fa93997ccb6bbbb3d98bbb00e99
codingbycoding <wangyongjiancom@foxmail.com>
2013-04-10 20:36:51 +0800

    for stl study

---
+++ stl/stlmain.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+
+int main()
+{
+	std::cout << "review cpp..." << std::endl;
+	
+	cout << "Run __FILE__:" << __FILE__ << " __LINE__:" << __LINE__ << " __DATE__:" << __DATE__ << " __TIME__:" << __TIME__ << endl;
+	
+	return 0;
+}
