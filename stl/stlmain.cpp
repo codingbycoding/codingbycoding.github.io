0ee0b4f52164c0f689bea53b29895bde5c289ed2
codingbycoding <wangyongjiancom@foxmail.com>
2013-04-11 00:05:02 +0800

    new default_run_target in run_target.mk.

--- stl/stlmain.cpp
+++ stl/stlmain.cpp
@@ -2,12 +2,48 @@
 #include <cassert>
 using namespace std;
 
+const static int intArg1 = 5;
+const static int intArg2 = 15;
+const static long longArg1 = 5;
+const static long longArg2 = 15;
+
+const static int intMagic1 = 10;
+const static int intMagic2 = 20;
+const static float floatMagic1 = 10.0;
+const static float floatMagic2 = 20.0;
+
+template<typename T>
+T sum(T arg1, T arg2)
+{
+	return arg1 + arg2;
+}
+
+/*
+nontype template function parameters.
+*/
+template<typename T, int magic>
+T sum(T arg1, T arg2)
+{
+	return arg1 + arg2 + magic;
+}
+
+
+//'float' is not a valid type for a template constant parameter
+/*template<typename T, float magic>
+T sum(T arg1, T arg2)
+{
+	return arg1 + arg2 + magic;
+}*/
 
 int main()
 {
-	std::cout << "review cpp..." << std::endl;
-	
-	cout << "Run __FILE__:" << __FILE__ << " __LINE__:" << __LINE__ << " __DATE__:" << __DATE__ << " __TIME__:" << __TIME__ << endl;
+	std::cout << "stlmain cpp..." << std::endl;
+
+	std::cout << "::sum(intArg1, intArg2) :" << ::sum<int>(intArg1, intArg2) << std::endl;
+	std::cout << "::sum(intArg1, intArg2, intMagic1) :" << ::sum<int, intMagic1>(intArg1, intArg2) << std::endl;
+
+	//error: 'float' is not a valid type for a template constant parameter
+	//std::cout << "::sum(floatArg1, floatArg2, floatMagic1) :" << ::sum<float, intMagic1>(floatArg1, floatArg2) << std::endl;
 	
 	return 0;
 }
