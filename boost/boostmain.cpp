154d3d0f66d4f1d4565c3cdb9ba935ac2901d9dc
codingbycoding <wangyongjiancom@foxmail.com>
2013-06-15 12:16:02 +0800

    boost begin from of

--- boost/boostmain.cpp
+++ boost/boostmain.cpp
@@ -1,7 +1,15 @@
+//g++ -Id:/boost_1_47_0/ -Bd:/boost_1_47_0/stage/lib -Lboost_date_time_vc100-mt-gd-1_47
+
 #include <iostream>
+#include <string>
+
+#include <boost/timer.hpp>
+
 
 int main()
 {
   std::cout << "boostmain.cpp" << __FILE__ << __LINE__ << std::endl;
+  std::string str;
+
   return 0;
 }
