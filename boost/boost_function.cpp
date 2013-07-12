fe18890c9aff569a895dd976ca187e0ff903fd55
codingbycoding <wangyongjiancom@foxmail.com>
2013-07-12 13:23:53 +0100

    boost bind function

--- boost/boost_function.cpp
+++ boost/boost_function.cpp
@@ -5,13 +5,22 @@
 #include <cmath>
 #include <typeinfo>
 
+#include <algorithm>
+#include <sstream>
+
 #include "boost/utility/result_of.hpp"
 
 #include "boost/ref.hpp"
 #include "boost/typeof/typeof.hpp"
 
-
 #include "boost/bind.hpp"
+
+#include "boost/assign.hpp" 
+
+#include "boost/utility.hpp"
+#include "boost/typeof/typeof.hpp"
+
+#include "boost/function.hpp"
 
 
 typedef double (*Func)(double);
@@ -47,6 +56,94 @@ public:
   }
 };
 
+
+class TestClass
+{
+private:
+  int _a;
+public:
+  TestClass():_a(0){}
+  void print()
+  {
+    std::cout << "_a: " << ++_a << std::endl;
+  }
+
+  void print(std::string str )
+  {
+    std::cout << "str: " << str << std::endl;
+  }
+
+  void print(int a )
+  {
+    std::cout << "a: " << a << std::endl;
+  }
+
+  void print(std::string str1, std::string str2 )
+  {
+    std::cout << "str1: " << str1 << "   str2: " << str2 << std::endl;
+  }
+};
+
+
+template<class T1, class T2, class T3>
+class DemoClass
+{
+private:
+  T1 _a;
+  T2 _b;
+  T3 _c;
+  
+public:
+  DemoClass(int a=0, int b=0, int c=0):_a(a), _b(b), _c(c){}
+  DemoClass(T1 a, T2 b, T3 c):_a(a), _b(b), _c(c){}  
+  void print()
+  {
+    std::cout << "_a: " << _a << "   _b: " << _b << "   _c: "  << _c << std::endl;
+  }
+
+  void print(T1 t1, T2 t2, T3 t3)
+  {
+    std::cout << "t1: " << t1 << "   t2: " << t2 << "   t3: "  << t3 << std::endl;
+  }
+  
+  template<class TT1> 
+  void print(TT1 t1)
+  {
+    std::cout << "t1: " << t1 << std::endl;
+  }
+  
+  template<class TT1, class TT2>
+  void print(T1 t1, T2 t2)
+  {
+    std::cout << "t1: " << _a << "   _b: " << _b << "   _c: "  << _c << std::endl;
+  }
+
+  template<class TT1, class TT2, class TT3>  
+  void print(TT1 t1, TT2 t2, TT3 t3)
+  {
+    std::cout << "t1: " << t1 << "   t2: " << t2 << "   t3: "  << t3 << std::endl;
+  }
+
+  
+};
+
+// template<>
+// class DemoClass
+// {
+// private:
+//   int  _a;
+//   int  _b;
+//   int  _c;
+  
+// public:
+//   DemoClass(int a=0, int b=0, int c=0):_a(a), _b(b), _c(c){}
+//   void print()
+//   {
+//     std::cout << "_a: " << _a << "   _b: " << _b << "   _c: "  << _c << std::endl;
+//   }
+
+// };
+
 template <class T>
 void print(T t)
 {
@@ -54,11 +151,28 @@ void print(T t)
 }
 
 
+
 template <class T1, class T2>
 void print(T1 t1, T2 t2)
 {
   std::cout << "arg1: " << t1 << "  arg2: " << t2 << std::endl;
 }
+
+
+struct printer
+{
+  typedef std::string result_type; //without this then boost::bind<typename> 
+  // void operator()(std::string str)
+  result_type operator()(int str)  
+  {
+    // print<std::string, int>(std::string("abc"), 123);
+    // print(str, 123);
+    std::stringstream ss;
+    ss <<  "arg: " << str << std::endl;
+    return ss.str(); 
+  }
+
+};
 
 int main()
 {
@@ -118,5 +232,68 @@ int main()
 
   boost::bind(print<std::string, int>, _1, _2)("abc", 123);
   boost::bind(print<std::string, std::string>, _1, "jack")("abc", 123);
+
+  DemoClass<std::string, std::string, std::string> demo("a", "b", "c");
+  // DemoClass demoS;
+  // boost::bind(&DemoClass<std::string, std::string, std::string>::print, &demo, "jack", _1, _2)("abc", "123");
+  TestClass test;
+  boost::bind(&TestClass::print, test)();  
+  boost::bind(&TestClass::print, test, "Hello", _1)("World");
+  boost::bind(&TestClass::print, &test, "Hello", _1)("World");
+  // boost::bind(DemoClass<std::string, std::string, std::string>::print<std::string, std::string, int>, &demo, "jack", _1, _2)("abc", 123);  
+
+
+  using namespace  boost::assign;
+  std::list<int> l = list_of(1)(2)(3)(4)(5);
+  for(std::list<int>::iterator it = l.begin(); it!=l.end(); ++it) 
+  // BOOST_FOREACH(BOOST_AUTO(it, l.begin()), l)
+    {
+      std::cout << *it << std::endl; 
+    }
   
+  // std::for_each( l.begin(), l.end(),  boost::bind(&TestClass::print, test, _1) );
+  // std::for_each( l.begin(), l.end(),  printer );
+  std::cout << "for_each begin..." << std::endl;
+  std::for_each( l.begin(), l.end(),  printer() );
+  // std::cout << boost::bind<std::string>(printer(), _1)(998) << std::endl; //without result_type in functor
+  std::cout << boost::bind(printer(), _1)(998) << std::endl;  //with result_type in functor
+
+  BOOST_AUTO(x_f, boost::bind(std::greater<int>(), _1, _2) );
+  std::cout << x_f(20,30) << std::endl;
+	     
+  boost::function<int(int,int)> func2;
+  func2 = std::max<int>;
+  if(func2)
+    {
+      std::cout << "func2(10,20): " << func2(10,20) << std::endl;
+      std::cout << std::endl;
+    }
+  else
+    {
+      std::cout << std::endl;
+    }
+  
+  func2 = 0;
+  
+  if(func2.empty())
+    {
+      std::cout << "func2 empty" << std::endl;
+    }
+  else
+    {
+      std::cout << "func2 not empty." << std::endl;
+    }
+
+
+  boost::function<void(TestClass&, std::string, std::string)> func2_mem1;
+  TestClass tf1;
+  func2_mem1 = boost::bind(&TestClass::print, _1, _2, _3);
+  func2_mem1(tf1, "Come","On");
+
+  std::cout << "seperator******************" << std::endl;
+  
+  boost::function<void(std::string, std::string)> func2_mem2;
+  TestClass tf2;  
+  func2_mem2 = boost::bind(&TestClass::print, &tf2,  _1, _2);    
+  func2_mem2("Come","On");  
 }
