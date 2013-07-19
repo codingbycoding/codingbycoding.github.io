7b0149e614ad749dc9d641ae1f05128f50230dc5
codingbycoding <wangyongjiancom@foxmail.com>
2013-07-19 14:58:41 +0100

    boost python

---
+++ boost/boost_python.cpp
@@ -0,0 +1,11 @@
+//g++ -g  -ID:\boost_1_47_0 -ID:\Python26\include -o  boost_python.exe boost_python.cpp D:\Python26\libs\libpython26.a
+
+#define BOOST_PYTHON_SOURCE
+#include "boost/python.hpp"
+
+int main()
+{
+  
+  std::cout << std::endl;
+  return 0;
+}
