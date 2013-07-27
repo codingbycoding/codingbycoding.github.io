8f76c610448e7d5240feef27fc20174142d4f92e
codingbycoding <wangyongjiancom@foxmail.com>
2013-07-27 12:30:39 +0800

    commit to merge.

--- python/pytest.py
+++ python/pytest.py
@@ -5,9 +5,12 @@ def FuncTest(x):
 
 
 class ClassTest(object):
-
+    """   """
     def __init__(self):
         print("class ClassTest: " + "__init__")
+
+    def showName(self):
+        print "ClassTest"
 
         
 
@@ -15,4 +18,41 @@ class ClassTest(object):
 print "HelloWorld"        
 FuncTest("No")    
 c1 = ClassTest()    
+c1.showName()
 
+import sys
+print sys.platform
+
+
+tuple1 = ('jack', 200, 'yes', 300)
+list1 = ['listnumber', 300, 200, 'ok']
+print tuple1
+print list1
+
+# if true :
+#     print jack
+print dir(list)
+
+
+i=0
+while (i < 30):
+    i+=1
+    print '*',
+    # print '*'
+print dir(tuple)
+
+
+print type(('abc'))
+print type(('abc', ))
+
+dict1={'IP':'127.0.0.1', 'Port':'6020'}
+print dict1
+
+for key in dict1:
+    print 'key=%s value=%s' % (key, dict1[key])
+
+
+
+
+ax = lambda x1, x2=10 : x1 + x2
+print ax(20)
