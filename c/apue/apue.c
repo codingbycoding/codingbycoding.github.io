2f28e204e05e74ce5052c449df867cd219105e16
codingbycoding <wangyongjiancom@foxmail.com>
2013-10-14 05:57:59 -0700

    pid sigaction

--- c/apue/apue.c
+++ c/apue/apue.c
@@ -5,12 +5,17 @@
 //c89 true false is not define 
 //gcc -std=c89 apue.c -o apue.linux
 
+
+//stdio, erno, cwd  path, sigaction, time, atexit
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/times.h>
 
 #ifndef true
 #define true 1
@@ -35,10 +40,22 @@ printf("sigaction_handle_fun signo:%d\n", signo);
 GbStop = true;
 }
 
+void atexit_fun1()
+{
+printf("atexit_fun1\n");
+}
+
+void atexit_fun2()
+{
+printf("atexit_fun2\n");
+}
 
 int main(int argc, char* argv[])
 {
 printf("Program %s Begin\n", argv[0]);
+struct tms begintime;
+clock_t begreal = times(&begintime);
+
 struct sigaction act, oact;
 sigemptyset(&act.sa_mask);
 act.sa_flags = SA_SIGINFO;
@@ -65,11 +82,66 @@ perror("putc error");
 }
 }
 
-while(!GbStop)
-	/* while(1)  //C99 define true C89 didn't define */
+
+time_t t = time(NULL);
+struct tm* localtm = localtime(&t);
+printf("Time:%s\n", asctime(localtm));
+
+printf("getlogin:%s\n", getlogin());
+
+
+atexit(atexit_fun1);
+atexit(atexit_fun2);
+
+printf("getpid():%d\n", getpid());
+
+pid_t pid;
+
+if( (pid = fork()) < 0)
 {
+perror("fork error\n");
+}
+else if(pid == 0)
+{
+printf("great this is child process created by fork \n");
+printf("getpid():%d\n", getpid());
+printf("getppid():%d\n", getppid());
+printf("getpgrp():%d\n", getpgrp());
+}
+else
+{
+printf("parent process is here.\n");
 
 }
-  
+
+/* extern char** environ; */
+
+/* char** env = environ; */
+/* while(*env) */
+/* { */
+/* ++env; */
+/* printf("%s\n", *env); */
+/* } */
+
+
+
+/* while(environ[]) */
+/* { */
+/* printf(""); */
+/* } */
+
+/* while(!GbStop) */
+/* 	/\* while(1)  //C99 define true C89 didn't define *\/ */
+/* { */
+
+/* } */
+struct tms endtime;
+clock_t endreal = times(&endtime);
+
+printf("real:%f\n"
+"user:%f\n"
+"sys:%f\n",
+	       (float)(endreal-begreal)/CLOCKS_PER_SEC, (float)(endtime.tms_utime-begintime.tms_utime)/CLOCKS_PER_SEC, (float)(endtime.tms_stime-begintime.tms_stime)/CLOCKS_PER_SEC );
+
 return 0;
 }
