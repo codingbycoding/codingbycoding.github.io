d59fffd00a4113cd0c4f4447272074f2596d39c2
codingbycoding <adam@server-dev.DOBE>
2014-12-21 19:56:45 +0800

    1.javascript lottery 2.testrobot in redis

--- cpp/libs/protobuf/test.cpp
+++ cpp/libs/protobuf/test.cpp
@@ -3,7 +3,7 @@
 //g++ test.cpp gen/*.cc -lprotobuf -o test.linux
 
 //option optimize_for = LITE_RUNTIME; 
-//g++ test.cpp gen/*.cc -lprotobuf-lite -o test.linux
+//g++ test.cpp gen/*.cc -I/usr/local/include -lprotobuf-lite -o test.linux
 
 
 #include <iostream>
@@ -11,7 +11,16 @@
 
 #include "gen/testproto.pb.h"
 
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
 #include <sstream>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int main(int argc, char* argv[]) {
 
@@ -40,12 +49,9 @@ int main(int argc, char* argv[]) {
     std::stringstream ss;
     
     for(int i=0; i<desc->field_count(); i++) {
-        const FieldDescriptor* fieldDesc = desc->field(i);
+        const google::protobuf::FieldDescriptor* fieldDesc = desc->field(i);
         ss << i << ": name:" << fieldDesc->name() << std::endl;
     }
-
-    
-    
     
 
 
@@ -85,6 +91,14 @@ int main(int argc, char* argv[]) {
 
     testpackagebase::TimeStamp* ti = new testpackagebase::TimeStamp();
     ti->set_time(10000);
+	ti->mutable_ext_data()->set_ext_int1(2222);
+	ti->mutable_ext_str()->set_ext_str_val("ext_str_val");
+	ti->set_opt_name("opt_name");
+
+	ti->add_rep_name("rep_name1");
+	ti->add_rep_name("rep_name2");
+	ti->add_rep_name("rep_name3");
+	
     req.set_allocated_time(ti);
     
     std::cout << "time:" << req.time().time() << std::endl;
@@ -92,7 +106,30 @@ int main(int argc, char* argv[]) {
     // std::string str;
     // req.SerializeToString(&str);
     // std::cout << "str:" << str << std::endl;
-    
+
+
+
+	std::string str_output;
+	google::protobuf::TextFormat::PrintToString(*ti, &str_output);
+	std::cout << "str_output: "
+			  << std::endl
+			  << std::endl
+			  << str_output << std::endl;
+
+	
+	// int fd = open("pbfile", O_CREAT, S_IRWXU);
+	int fd = open("pbfile", O_TRUNC);
+	if(-1 == fd) {
+		std::cerr << "open failed! errno:" << strerror(errno) << std::endl;
+	}
+	
+	google::protobuf::io::ZeroCopyOutputStream* raw_output = new google::protobuf::io::FileOutputStream(fd);
+
+	if(!google::protobuf::TextFormat::Print(*ti, raw_output)) {
+		std::cerr << "open failed! errno:" << strerror(errno) << std::endl;
+	}
+
+	close(fd);
     google::protobuf::ShutdownProtobufLibrary();
     return EXIT_SUCCESS;
 
