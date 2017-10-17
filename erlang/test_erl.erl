117f7367458aee4ed4aa66f0f18cab386feb4b1f
Wang Yongjian(Adam) <wangyongjiancom@foxmail.com>
2017-10-17 20:06:40 +0800

    test gen_server

---
+++ erlang/test_erl.erl
@@ -0,0 +1,63 @@
+-module(test_erl).
+
+-record(amap, {akey, bvalue}).
+-record(state, {alist = []}).
+
+-behaviour(gen_server).
+
+-export([init/1, start/0, start_link/4, stop/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
+
+-export([test/0, test_replace/1, print/0]).
+
+init(_ABC) ->
+	{ok, #state{alist = [#amap{akey = 10, bvalue = a}, #amap{akey = 2, bvalue = b}, #amap{akey = 3, bvalue = c}]}}.
+	%%abc.
+	
+test() ->		
+	gen_server:call(?MODULE, {test}).
+
+print() ->		
+	gen_server:call(?MODULE, {print}).	
+			   
+test_replace(#state{alist = AList}) ->
+	ATuple = #amap{akey = 100, bvalue = z},
+	lists:keyreplace(10, 3, AList, ATuple).
+
+start() ->
+	gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+	
+	
+start_link(_Name, _Mod, _Args, _Options) ->
+	{ok, self()}.
+	
+handle_call({test}, _From, State) ->
+	NewState = lists:sort(fun(A, B) ->
+				A#amap.akey < B#amap.akey end,				
+			   State#state.alist),
+		
+	{reply, ok, NewState};
+handle_call({print}, _From, State) ->
+	lists:foreach(fun(A) -> 
+			io:formart("{k:~p  } ", [A]) end,				
+			   State#state.alist),			
+	{reply, ok, State};		
+handle_call(_Request, _From, _State) ->
+	{reply, ok}.
+	
+handle_cast(_Request, _State) ->
+	ok.
+	
+handle_info(atom_a, _State) ->
+	io:format("xxxatoma~n"),
+	ok.
+
+	
+code_change(_OldVsn, _State, _Extra) ->
+    ok.
+	
+terminate(_Reason,_State) ->
+	ok.
+		
+stop(_Name) ->
+	ok.
+
